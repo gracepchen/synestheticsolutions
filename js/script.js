208785cb298e9e432044b37b42e2ffffe10a30b3
Grace Chen <gpc001@ucsd.edu>
2018-02-26 12:13:14 -0800

    navbar, bg strips

--- js/script.js
+++ js/script.js
@@ -18,3 +18,37 @@ $(function() {
 // Change the speed to whatever you want
 // Personally i think 1000 is too much
 // Try 800 or below, it seems not too much but it will make a difference
+
+$(function () {
+  var lastScrollTop = 100;
+  var $navbar = $('.navbar');
+
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+
+    if (st > lastScrollTop) { // scroll down
+      
+      // use this is jQuery full is used
+      $navbar.fadeOut()
+      
+      // use this to use CSS3 animation
+      // $navbar.addClass("fade-out");
+      // $navbar.removeClass("fade-in");
+      
+      // use this if no effect is required
+      // $navbar.hide();
+    } else { // scroll up
+      
+      // use this is jQuery full is used
+      $navbar.fadeIn()
+      
+      // use this to use CSS3 animation
+      // $navbar.addClass("fade-in");
+      // $navbar.removeClass("fade-out");
+      
+      // use this if no effect is required
+      // $navbar.show();
+    }
+    lastScrollTop = st;
+  });
+});
